593e3653ec90a0017964ba70f22da18bfa34424f
lliksis <40835286+lliksis@users.noreply.github.com>
2023-01-12 15:45:32 +0100

    Removed web, windows, macos, linux projects

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Postmaster',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -23,9 +23,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.cyan,
       ),
-      home: const BottomNavigation(title: 'Flutter Demo Home Page'),
+      home: const BottomNavigation(title: 'Postmaster'),
     );
   }
 }
